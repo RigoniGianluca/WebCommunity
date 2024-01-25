ec9d4f13e9d71bd9b793589b921e715e4ba88a0e
Rigoni <8293360@itisrossi.vi.it>
2024-01-25 20:53:57 +0100

    .

--- Login.php
+++ Login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>BOH</title>
+        <title>LOGIN</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <?php
 
@@ -28,67 +28,14 @@
                     }
                 }
                 else{
-                    echo "Utente Errato!";
+                    echo '<div class="bg-slate-950 text-white font-bold text-xl text-center">Credenziali utente errate</div>';
+                    break;
                 }
             }
         }
         ?>
     </head>
     <body>
-    <nav class="bg-gray-800">
-        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div class="relative flex h-16 items-center justify-between">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    <!-- Mobile menu button-->
-                    <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                        <span class="absolute -inset-0.5"></span>
-                        <span class="sr-only">Open main menu</span>
-                        <!--
-                          Icon when menu is closed.
-
-                          Menu open: "hidden", Menu closed: "block"
-                        -->
-                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                        <!--
-                          Icon when menu is open.
-
-                          Menu open: "block", Menu closed: "hidden"
-                        -->
-                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                    <div class="flex flex-shrink-0 items-center">
-                        <img class="h-8 w-auto" src="./images/foto.png" alt="Your Company">
-                    </div>
-                    <div class="hidden sm:ml-6 sm:block">
-                        <div class="flex space-x-4">
-                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <button type="button" name="home" id="home" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</button>
-                            <button type="button" name="perTe" id="perTe" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Per Te</button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Profile dropdown -->
-                <div class="relative ml-3">
-                    <div>
-                        <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                            <span class="absolute -inset-1.5"></span>
-                            <span class="sr-only">Open user menu</span>
-                            <img class="h-8 w-8 rounded-full" src="https://img.favpng.com/1/4/11/portable-network-graphics-computer-icons-google-account-scalable-vector-graphics-computer-file-png-favpng-HScCJdtkakJXsS3T27RyikZiD.jpg" alt="">
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-    </nav>
-
 
         <div class="min-h-screen  grid bg-slate-950">
           <div class="flex flex-row flex-auto justify-itmes-center w-90">
@@ -106,9 +53,9 @@
                         <div class="bg-slate-950 flex flex-col w-80 border border-gray-900 rounded-lg px-8 py-8">
                             <form class="flex flex-col space-y-8 mt-10" action="./Login.php" method="POST">
                                 <label class="text-center text-white text-xl font-semibold">Inserisci le credenziali</label>
-                                <label class="font-bold text-lg text-white " >Username:</label> 
+                                <label class="font-bold text-lg text-white " >Username:</label>
                                 <input type="text" id="username" name="username" class="border rounded-lg py-3 px-3 mt-4 bg-slate-950 border-indigo-600 placeholder-white-500 text-white">
-                                <label class="font-bold text-lg text-white">Password:</label> 
+                                <label class="font-bold text-lg text-white">Password:</label>
                                 <input type="password" id="username" name="password" placeholder="*****" class="border rounded-lg py-3 px-3 bg-slate-950 border-indigo-600 placeholder-white-500 text-white">
                                 <button class="border-2 border-indigo-600 hover:bg-slate-700/50 bg-slate-950 text-white rounded-lg py-3 font-semibold">Login</button>
                                 <label class="text-white font-semibold text-gl">Non hai un account? <a href="./Register.php" class="font-bold">Registrati</a></label>
